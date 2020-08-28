0ec1a156fdaa956ce82cbfa843678dfba4c908c1
Jeremy Huot <logflume@live.com>
2020-08-27 19:33:34 -0600

    Co-authored-by: Sara Irving  <sarakirving@gmail.com>

--- src/db/schema/create.sql
+++ src/db/schema/create.sql
@@ -1,35 +1,38 @@
-DROP TABLE IF EXISTS available_interviewers CASCADE;
-DROP TABLE IF EXISTS interviews CASCADE;
-DROP TABLE IF EXISTS interviewers CASCADE;
-DROP TABLE IF EXISTS appointments CASCADE;
-DROP TABLE IF EXISTS days CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS activites CASCADE;
+DROP TABLE IF EXISTS messages CASCADE;
+DROP TABLE IF EXISTS activity_participants CASCADE;
 
-CREATE TABLE days (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE appointments (
-  id SERIAL PRIMARY KEY NOT NULL,
-  time VARCHAR(255) NOT NULL,
-  day_id INTEGER REFERENCES days(id) ON DELETE CASCADE
-);
-
-CREATE TABLE interviewers (
+CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   avatar VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE interviews (
+CREATE TABLE activities (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  name VARCHAR(255) NOT NULL,
+  num_of_participants INTEGER,
+  city VARCHAR(255) NOT NULL,
+  frequency VARCHAR(255) NOT NULL,
+  days_available VARCHAR(255),
+  timeframe VARCHAR(255),
+  location VARCHAR(255),
+  skill_tag VARCHAR(555),
+  tags VARCHAR(800),
+  created_at 
+);
+
+CREATE TABLE messages (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  avatar VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE activity_participants (
   id SERIAL PRIMARY KEY NOT NULL,
   student VARCHAR(255) NOT NULL,
   interviewer_id INTEGER REFERENCES interviewers(id) ON DELETE CASCADE,
   appointment_id INTEGER UNIQUE REFERENCES appointments(id) ON DELETE CASCADE
-);
-
-CREATE TABLE available_interviewers (
-  id SERIAL PRIMARY KEY NOT NULL,
-  day_id INTEGER REFERENCES days(id) ON DELETE CASCADE,
-  interviewer_id INTEGER REFERENCES interviewers(id) ON DELETE CASCADE
 );
